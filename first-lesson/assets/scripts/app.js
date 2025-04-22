23e1ab2639ff0e014c96a8f632db71a895613f6b
Dimitar Dimitrov <d.dimitrov@anakatech.com>
2025-04-22 17:18:09 +0300

    Inital commit for all calculator operations

--- first-lesson/assets/scripts/app.js
+++ first-lesson/assets/scripts/app.js
@@ -1,8 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// Getting the user input value
 function getUserNumberInput() {
     return parseInt(userInput.value);
+}
+
+// Create and write logs
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
 }
 
 
@@ -10,12 +17,36 @@ function add() {
     // Convert number to string:
     // currentResult = currentResult.toString() + parseInt(userInput.value);
 
-    const userInputValue = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${userInputValue}`;
+    const enteredNumber = getUserNumberInput();
     // `${...}` - convert param in the brackets to string (no need to parse)
+    const initialResult = currentResult;
 
-    currentResult = currentResult + userInputValue;
-    outputResult(currentResult, calcDescription);
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
