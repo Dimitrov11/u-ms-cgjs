2dd338c84fa998e24da162a787eedd38937e7b30
Dimitar Dimitrov <d.dimitrov@anakatech.com>
2025-04-22 16:41:05 +0300

    Initial commit of finish the add function in our calculator

--- first-lesson/assets/scripts/app.js
+++ first-lesson/assets/scripts/app.js
@@ -2,7 +2,10 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-    currentResult = currentResult + userInput.value;
+    // Convert number to string:
+    // currentResult = currentResult.toString() + parseInt(userInput.value);
+
+    currentResult = currentResult + parseInt(userInput.value);
     outputResult(currentResult, '');
 }
 
