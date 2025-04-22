10a7cb9267b6a50333178d7c2b3cb067e40f0f92
Dimitar Dimitrov <d.dimitrov@anakatech.com>
2025-04-22 17:01:38 +0300

    Initial commit of parse the user input number as function

--- first-lesson/assets/scripts/app.js
+++ first-lesson/assets/scripts/app.js
@@ -1,12 +1,21 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+
 function add() {
     // Convert number to string:
     // currentResult = currentResult.toString() + parseInt(userInput.value);
 
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, '');
+    const userInputValue = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${userInputValue}`;
+    // `${...}` - convert param in the brackets to string (no need to parse)
+
+    currentResult = currentResult + userInputValue;
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
