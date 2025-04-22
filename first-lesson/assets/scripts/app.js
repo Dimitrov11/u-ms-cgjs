41f4262a29f15c41dde892145c4f4996cee03653
Dimitar Dimitrov <d.dimitrov@anakatech.com>
2025-04-22 12:06:35 +0300

    Inital commit of function declaration

--- first-lesson/assets/scripts/app.js
+++ first-lesson/assets/scripts/app.js
@@ -1,7 +1,15 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = currentResult + 10; 
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert(`The result is: ${result}`);
+}
 
-let calculationDescription = '(' + currentResult + ' + 10) * 3 /2 - 1';
+// add();
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 /2 - 1`;
 
 outputResult(currentResult, calculationDescription);
